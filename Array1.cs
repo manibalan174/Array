07763208471cea2fc9881fbd839f7bb1d5b1e7dc
manibalan174 <45997825+manibalan174@users.noreply.github.com>
2021-05-10 11:29:02 +0530

    Add files via upload

---
+++ Array1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        public void Array(int[] arry)
+        {
+            Console.WriteLine("Array Exection");
+            for(int i=0; i<arry.Length;i++)
+            {
+                Console.WriteLine(arry[i]);
+            }
+        }
+        public static void Arry1()
+        {
+            Console.WriteLine("Ddeclareing Array VAluse");
+            int[] Number = new int[5];
+              Number[0]=1;
+              Number[2]=3;
+              Number[4]=5;
+              for(int i =0; i< Number.Length;i++)
+              {
+                  Console.WriteLine(Number[i]);
+              }
+            
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Program P = new Program();
+            int[] arry1={10,20,30,40,50};
+            int[] arry2={1,2,3,4,5};
+            P.Array(arry1);
+            P.Array(arry2);
+            Arry1();
+        }
+    }
+}
---
+++ Test.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ArrayTest
+{
+    class Program
+    {
+       
+       public void DimensionalArray()
+       {
+       
+       }
+        
+        public static void UserArray()
+        {
+  int[] arr = new int[10]; 
+    int i;  
+       Console.Write("\n\nRead and Print elements of an array:\n");
+       Console.Write("-----------------------------------------\n");	
+  
+    Console.Write("Input 10 elements in the array :\n");  
+    for(i=0; i<10; i++)  
+    {  
+	    Console.Write("element - {0} : ",i);
+	    arr[i] = Convert.ToInt32(Console.ReadLine());  	
+         if(arr[i] == 2)
+           {
+            break;
+         }	
+    }  
+  if(arr[i] == 2 )
+  {
+      Console.WriteLine("HEllo MAchi");
+  }
+    Console.Write("\nElements in array are: ");  
+    for(i=0; i<arr.Length; i++)  
+    {  
+        Console.Write("{0}  ", arr[i]);  
+    } 
+    Console.Write("\n");
+        }
+        static void Main(string[] args)
+        {
+           UserArray();
+          // DimensionalArray();
+        }
+
+    }
+}
