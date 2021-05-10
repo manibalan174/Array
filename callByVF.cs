4e8834372e1dc0542e6f0670e74b7ac7bae57d1e
manibalan174 <45997825+manibalan174@users.noreply.github.com>
2021-05-10 11:30:19 +0530

    Add files via upload

---
+++ callByVF.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CallByVF
+{
+    class Program
+    {
+        // public static void CallByValus(int n) // CAll Vy Valeus
+        // {
+        //      ++n;
+        //     Console.WriteLine("CAll By Values :"+n);
+        // }
+     public static void CAllByFef(ref int n)  // CAll Vy Refernce
+     {
+            ++n;
+           Console.WriteLine("CAll By Reference :"+n);
+     }
+
+        static void Main(string[] args)
+        {
+           
+            int i = 5;
+             Console.WriteLine("Initiazing the Values for i : "+i);
+            // CallByValus(i);
+               CAllByFef(ref i);
+             Console.WriteLine("After Passing : "+i);
+           // CallByVTest();
+        }
+    }
+}
